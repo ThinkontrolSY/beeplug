7a128f1728fa536b20443974cc7bf70efaa80d03
ShenYang <shenyang.sx@outlook.com>
2025-01-17 09:27:00 +0800

    s

--- entity_kpi.go
+++ entity_kpi.go
@@ -1,6 +1,7 @@
 package beeplug
 
 type EntityKpi struct {
+	Bucket string            `json:"bucket,omitempty" yaml:"bucket"`
 	Name   string            `json:"name,omitempty" yaml:"name"`
 	Fields map[string]any    `json:"fields,omitempty" yaml:"fields"`
 	Tags   map[string]string `json:"tags,omitempty" yaml:"tags"`
